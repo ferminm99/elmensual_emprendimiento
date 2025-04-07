40e0201149e0adf343b226d7634def61b7c2133f
Ferminm99 <ferminmorenoz99@gmail.com>
2025-04-07 16:00:46 -0300

    cors fix

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'login', 'logout', 'csrf-token', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://elmensual-emprendimiento.vercel.app','VITE_APP_URL','http://localhost:5173','APP_URL','http://localhost:8000'],
+    'allowed_origins' => ['https://elmensual-emprendimiento.vercel.app','http://localhost:5173','http://localhost:8000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
